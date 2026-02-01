cca0b8dd6293f445d2b651dd71c7620c543b41d2
mahdipoorma8-lang <mahdipoorma8@gmail.com>
2026-02-01 16:48:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ from dotenv import load_dotenv
 
 # ========= تنظیمات =========
 load_dotenv()  # خواندن متغیرهای محیطی
-BOT_TOKEN = os.getenv("BOT_TOKEN") or "توکن_بات_اینجا"
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "8217406460:AAFhmRdYqMbR5CKT2YsjDl6A-0gdixzTBW4"
+ADMIN_IDS = list(map(int, os.getenv("7506306837", "").split(",")))
 DB_NAME = "reports.db"
 
 # ساعت یادآوری به وقت ایران
-REMINDER_HOUR = 17
+REMINDER_HOUR = 20
 REMINDER_MINUTE = 0
 # ===========================
 
